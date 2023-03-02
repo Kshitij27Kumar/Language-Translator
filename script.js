2ba681cf0b6038261383af82c2c41a84eca2f53e
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-03-02 18:00:58 +0530

    Languages added to select tag and exchange icon implemented.

---
+++ script.js
@@ -0,0 +1,33 @@
+const fromText = document.querySelector('.from-text'),
+  toText = document.querySelector('.to-text'),
+  exchageIcon = document.querySelector('.exchange'),
+  selectTag = document.querySelectorAll('select'),
+  icons = document.querySelectorAll('.row i'),
+  translateBtn = document.querySelector('button')
+selectTag.forEach((tag, id) => {
+  for (let country_code in countries) {
+    let selected =
+      id == 0
+        ? country_code == 'en-GB'
+          ? 'selected'
+          : ''
+        : country_code == 'hi-IN'
+        ? 'selected'
+        : ''
+    let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`
+    tag.insertAdjacentHTML('beforeend', option)
+  }
+})
+exchageIcon.addEventListener('click', () => {
+  let tempText = fromText.value,
+    tempLang = selectTag[0].value
+  fromText.value = toText.value
+  toText.value = tempText
+  selectTag[0].value = selectTag[1].value
+  selectTag[1].value = tempLang
+})
+fromText.addEventListener('keyup', () => {
+  if (!fromText.value) {
+    toText.value = ''
+  }
+})
